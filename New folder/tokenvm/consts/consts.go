2b44742fd82ad96c0e2c8b5830f11ee06b193f63
Divyasingh03 <divya954.singh@gmail.com>
2024-09-28 12:30:27 +0530

    Update consts.go

--- New folder/tokenvm/consts/consts.go
+++ New folder/tokenvm/consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "DivyaSingh"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "DIVYA"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "DVY"
 )
 
 var ID ids.ID
